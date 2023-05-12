c6ab8cb7c8a14f8a701ac265e6d46513a1a4efd2
abbrshv <a.bobryshov@proton.me>
2023-05-12 19:50:57 +0300

    Modify script.js
    Implement additional buttons

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const displayText = document.querySelector(".display-text");
+const btnClear = document.querySelector(".btn-clear");
+const btnSign = document.querySelector(".btn-sign");
+const btnPercent = document.querySelector(".btn-percent");
+const btnComa = document.querySelector(".btn-coma");
 const numberButtons = document.querySelectorAll(".btn-num");
 const operatorButtons = document.querySelectorAll(".btn-op");
-const btnComa = document.querySelector(".btn-coma");
 
-let inputText = "";
-displayText.textContent = "0";
+let inputText;
 let operands = [];
 let operation;
 let result = 0;
@@ -24,13 +26,30 @@ function selectOperation(button) {
   }
 }
 
-function operate() {
-  if (operands.length === 2) {
-    result = operation(operands[0], operands[1]);
-    operands[0] = result;
-    displayText.textContent = result;
-    operands.pop();
+function clear(event) {
+  if (inputText === "") {
+    operands.length = 0;
+    operation = null;
   }
+  inputText = "";
+  displayText.textContent = "0";
+}
+
+function changeSign(event) {
+  inputText = -Number(inputText).toString();
+  displayText.textContent = inputText;
+}
+
+function convertPercent(event) {
+  inputText = Number(inputText) / 100;
+  displayText.textContent = inputText;
+}
+
+function operate() {
+  result = operation(operands[0], operands[1]);
+  operands[0] = result;
+  operands.pop();
+  displayText.textContent = result;
 }
 
 function handleNumBtnClick(event) {
@@ -43,7 +62,7 @@ function handleOperBtnClick(event) {
     operands.push(Number(inputText));
     inputText = "";
   }
-  operate();
+  if (operands.length === 2) operate();
   operation = selectOperation(event.target);
 }
 
@@ -52,6 +71,10 @@ function handleComaBtnClick(event) {
   displayText.textContent = inputText;
 }
 
+clear();
+btnClear.addEventListener("click", clear);
+btnSign.addEventListener("click", changeSign);
+btnPercent.addEventListener("click", convertPercent);
 numberButtons.forEach((btn) =>
   btn.addEventListener("click", handleNumBtnClick)
 );
